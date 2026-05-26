df0b44ab2459589637fb4fd2036e1f24b655d96e
YashB-byte <yash.behera@icloud.com>
2026-05-25 17:47:33 -0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Yash Behera on 5/25/26.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
